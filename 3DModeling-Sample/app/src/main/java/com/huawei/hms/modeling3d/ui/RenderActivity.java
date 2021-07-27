d6c542816c3bd014170ec3e12af0ac73eeff4915
fStrangerZ <fer.stranieri@gmail.com>
2021-07-27 18:27:34 +0200

    render feature with rajawali improved and rotation gesture

--- 3DModeling-Sample/app/src/main/java/com/huawei/hms/modeling3d/ui/RenderActivity.java
+++ 3DModeling-Sample/app/src/main/java/com/huawei/hms/modeling3d/ui/RenderActivity.java
@@ -6,23 +6,25 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.view.View;
 
 import com.huawei.hms.modeling3d.R;
 
 import org.rajawali3d.Object3D;
-import org.rajawali3d.lights.DirectionalLight;
 import org.rajawali3d.loader.LoaderOBJ;
+import org.rajawali3d.math.Quaternion;
+import org.rajawali3d.math.vector.Vector3;
 import org.rajawali3d.renderer.Renderer;
 import org.rajawali3d.view.SurfaceView;
 
 import java.io.File;
 
-public class RenderActivity extends AppCompatActivity {
+public class RenderActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
     public static final String EXTRA_FILE_PATH = "FILEPATH";
     private ObjRenderer renderer;
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +35,9 @@ public class RenderActivity extends AppCompatActivity {
 
         File objFile = findObjFile(getIntent().getExtras().getString(EXTRA_FILE_PATH));
         renderer = new ObjRenderer(this, objFile);
-
         view.setSurfaceRenderer(renderer);
-        view.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                renderer.onTouchEvent(motionEvent);
-                return false;
-            }
-        });
+
+        gestureDetector = new GestureDetector(this, this);
     }
 
     private File findObjFile(String path) {
@@ -59,9 +55,49 @@ public class RenderActivity extends AppCompatActivity {
         return null;
     }
 
-    class ObjRenderer extends Renderer {
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return true;
+    }
 
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float distanceX, float distanceY) {
+        renderer.setScroll(distanceX, distanceY);
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    class ObjRenderer extends Renderer {
         private final File objFile;
+        private Object3D object3D;
+
+        private boolean isScrolling = false;
+        private float xDistance = 0, yDistance = 0;
 
         public ObjRenderer(Context context, File objFile) {
             super(context);
@@ -69,20 +105,34 @@ public class RenderActivity extends AppCompatActivity {
         }
 
         @Override
+        protected void onRender(long elapsedRealtime, double deltaTime) {
+            if (isScrolling) {
+                Vector3 xAxis = renderer.getCurrentCamera().getOrientation().getXAxis();
+                Vector3 yAxis = renderer.getCurrentCamera().getOrientation().getYAxis();
+                Vector3 zAxis = renderer.getCurrentCamera().getOrientation().getZAxis();
+
+                //Method to rotate the 3D model
+                Quaternion x = new Quaternion(xAxis, -xDistance / 2);
+                Quaternion y = new Quaternion(zAxis, -yDistance / 2);
+                object3D.rotate(x.multiply(y));
+                this.isScrolling = false;
+            }
+            super.onRender(elapsedRealtime, deltaTime);
+        }
+
+        @Override
         protected void initScene() {
             try {
                 getCurrentScene().setBackgroundColor(Color.CYAN & Color.DKGRAY);
-                DirectionalLight key = new DirectionalLight(-4,-4,-4);
-                key.setPower(3);
-                getCurrentScene().addLight(key);
 
                 LoaderOBJ loader = new LoaderOBJ(this, this.objFile);
                 loader.parse();
-                Object3D obj = loader.getParsedObject();
-                getCurrentScene().addChild(obj);
+                object3D = loader.getParsedObject();
+                getCurrentScene().addChild(object3D);
+                object3D.setPosition(0,0,-2);
 
-                getCurrentCamera().setPosition(3,4,-5);
-                getCurrentCamera().setLookAt(obj.getPosition());
+                getCurrentCamera().setPosition(0, 0, 0);
+                getCurrentCamera().setLookAt(object3D.getPosition());
             } catch (Exception e) {
                 Log.d(getLocalClassName(), e.getMessage());
             }
@@ -95,7 +145,12 @@ public class RenderActivity extends AppCompatActivity {
 
         @Override
         public void onTouchEvent(MotionEvent event) {
+        }
 
+        public void setScroll(float xDistance, float yDistance) {
+            this.xDistance = xDistance;
+            this.yDistance = yDistance;
+            this.isScrolling = true;
         }
     }
 }
